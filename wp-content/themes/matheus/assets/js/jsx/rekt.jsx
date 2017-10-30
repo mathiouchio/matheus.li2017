f93d9fc92246b93ba4a2b1b05ca0a6ebdb6533bc
mathiouchio <contact@matheusdesain.com>
2017-10-30 13:15:43 -0400

    better UX toggle from + to -

--- wp-content/themes/matheus/assets/js/jsx/rekt.jsx
+++ wp-content/themes/matheus/assets/js/jsx/rekt.jsx
@@ -320,7 +320,30 @@ var rekt = {
 
           if (this.state) {
             this.state.posts.map(slide);
-            var print = (
+            if(this.state.expanded) {
+              expandButton = (
+                <a>
+                  <svg x="0px" y="0px" viewBox="0 0 40 40">
+                    <circle fill="none" cx="20" cy="20" r="12"></circle>
+                    <line x1="25.3" y1="20" x2="14.7" y2="20"></line>
+                  </svg>
+                  <span className="hero smler">Hide posts</span>
+                </a>
+              );
+            } else {
+              expandButton = (
+                <a>
+                  <svg x="0px" y="0px" viewBox="0 0 40 40">
+                    <line x1="25.3" y1="20" x2="14.7" y2="20"></line>
+                    <line x1="20" y1="14.7" x2="20" y2="25.3"></line>
+                    <circle fill="none" cx="20" cy="20" r="12"></circle>
+                  </svg>
+                  <span className="hero smler">Show posts</span>
+                </a>
+              );
+            }
+            
+            return (
               <div className="slider noslide" data-expanded={(this.state.expanded)}>
                 <div className="slides" ref="blogDOM">
                   <div className="slide">
@@ -329,17 +352,13 @@ var rekt = {
                         {output}
                       </ul>
                       <div className="expand" onClick={this.expandSection}>
-                        <a>
-                          <svg x="0px" y="0px" viewBox="0 0 40 40"><line x1="25.3" y1="20" x2="14.7" y2="20"></line><line x1="20" y1="14.7" x2="20" y2="25.3"></line><circle fill="none" cx="20" cy="20" r="12"></circle></svg>
-                          <span className="hero smler">{(this.state.expanded) ? 'Hide posts' : 'More posts'}</span>
-                        </a>
+                        {expandButton}
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             );
-            return print;
           } else {
             return null;
           }
