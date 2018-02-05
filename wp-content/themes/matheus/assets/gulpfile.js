0fcfdb9bce68c162e3ea0b486e37532cedb96f18
mathiouchio <contact@matheusdesain.com>
2018-02-04 22:13:01 -0500

    automation

--- wp-content/themes/matheus/assets/gulpfile.js
+++ wp-content/themes/matheus/assets/gulpfile.js
@@ -3,43 +3,62 @@ const autoprefixer = require('gulp-autoprefixer'),
       browserSync  = require('browser-sync').create(),
       concat       = require('gulp-concat'),
       del          = require('del'),
+      es           = require('event-stream'),
+      fs           = require('fs'),
+      exec         = require('gulp-exec'),
       gulp         = require('gulp'),
       gutil        = require('gulp-util'),
       jshint       = require('gulp-jshint'),
       livereload   = require('gulp-livereload'),
       minify       = require('gulp-minify'),
       rename       = require('gulp-rename'),
+      runSequence  = require('run-sequence'),
       sass         = require('gulp-sass'),
       sourcemaps   = require('gulp-sourcemaps'),
       stylish      = require('jshint-stylish'),
       ts           = require('gulp-typescript'),
-      tscConfig    = require('./tsconfig.json');
+      tscConfig    = require('./tsconfig.json'),
+      prompt       = require('gulp-prompt'),
       uglify       = require('gulp-uglify');
 
 const paths = {
-  node:     './node_modules/',
-  php:      ['../*.php', '../**/*.php'],
-  jsx:      'js/jsx/*.jsx',
-  ts:       'js/ts/*.ts',
-  jsMin:    '../js',
-  cssMin:   '../css',
-  sass:     'scss/*.scss',
-  jsConcat: ['js/libs/*.js',
-    'js/main.js',
-    'js/rekt.js',
-    'js/wipeskript.js'],
-  angularLibraries: [
-    '@angular/**/bundles/**',
-    'angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-    'core-js/client/shim.min.js',
-    'reflect-metadata/Reflect.js',
-    'systemjs/dist/system.src.js',
-    'rxjs/**/*.js',
-    'zone.js/dist/**'
-  ],
-  libs: ['react','react-dom', 'snapsvg', 'plyr', 'jquery'],
-  old: '../old'
-}
+        node:     './node_modules/',
+        php:      ['../*.php', '../**/*.php'],
+        jsx:      'js/jsx/*.jsx',
+        ts:       'js/ts/*.ts',
+        jsMin:    '../js',
+        cssMin:   '../css',
+        sass:     'scss/*.scss',
+        jsConcat: ['js/libs/*.js',
+          'js/main.js',
+          'js/rekt.js',
+          'js/wipeskript.js'],
+        angularLibraries: [
+          '@angular/**/bundles/**',
+          'angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+          'core-js/client/shim.min.js',
+          'reflect-metadata/Reflect.js',
+          'systemjs/dist/system.src.js',
+          'rxjs/**/*.js',
+          'zone.js/dist/**'
+        ],
+        libs: ['react','react-dom', 'snapsvg', 'plyr', 'jquery'],
+        old: '../old'
+      },
+      options = {
+        exec: {
+          options : {
+            continueOnError: false,  // default = false, true means don't emit error event
+            pipeStdout:      false,  // default = false, true means stdout is written to file.contents
+          },
+          reportOptions : {
+            err:             true,   // default = true, false means don't write err
+            stderr:          true,   // default = true, false means don't write stderr
+            stdout:          true    // default = true, false means don't write stdout
+          }
+        }
+      };
+
 
 const browserSyncProps = (function(){
   return {
@@ -95,6 +114,34 @@ gulp.task('clean-dist', function(cb){
     'js/angular',
     '../css/libs'
   ], {force: true});
+});
+
+gulp.task('git-reset', function(){
+  return gulp.src('commitmsg')
+    .pipe(exec('git reset', options.exec.options))
+    .pipe(exec.reporter(options.exec.reportOptions));
+});
+
+gulp.task('git-status', function(){
+  return gulp.src('commitmsg')
+    .pipe(exec('git status', options.exec.options))
+    .pipe(exec.reporter(options.exec.reportOptions));
+});
+
+gulp.task('git-add', function(){
+  return gulp.src('commitmsg')
+    .pipe(exec('git add .', options.exec.options));
+});
+
+gulp.task('git-commit', function(){
+  return gulp.src('commitmsg')
+    // .pipe(es.stringify())
+    .pipe(exec('git commit -m "automation"', options.exec.options))
+    .pipe(exec.reporter(options.exec.reportOptions));
+});
+
+gulp.task('git', function (cb) {
+  runSequence('git-reset', 'git-status', 'git-add', 'git-commit');
 });
 
 gulp.task('concat', function(){
@@ -199,4 +246,5 @@ gulp.task('watch', function() {
   gulp.watch(['scss/*.scss','scss/partials/*.scss'], ['sass']);
 });
 
+gulp.task('compile', ['babel','ts','sass']);
 gulp.task('default', ['browser-sync','babel','ts','sass','watch']);
